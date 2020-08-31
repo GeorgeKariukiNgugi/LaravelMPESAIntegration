80042a288ad0633bcb98f58cc61c75c6a7544a06
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-08-31 23:09:24 +0300

    Added New Implementation

--- app/Http/Controllers/newImplementation.php
+++ app/Http/Controllers/newImplementation.php
@@ -86,7 +86,7 @@ class newImplementation extends Controller
   
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer'.$this->generateAccessToken())); //setting custom header
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer'.$this->generateAccessTokens())); //setting custom header
         
         
         $curl_post_data = array(
