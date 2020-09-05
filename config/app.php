9b39321014b3a904b6eef9cfbc7c853d5c6ce799
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-09-05 06:52:36 +0300

    Added the channels and events

--- config/app.php
+++ config/app.php
@@ -227,7 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Mpesa'=> Safaricom\Mpesa\MpesaServiceProvider::class
+        'Mpesa'=> Safaricom\Mpesa\MpesaServiceProvider::class,
+        'Pusher' => Pusher\Pusher::class,
 
     ],
 
