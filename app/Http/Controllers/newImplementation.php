5b4b0c922ea0f89d291b13758ccf0b199cc05c15
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-09-07 12:39:11 +0300

    Added changes

--- app/Http/Controllers/newImplementation.php
+++ app/Http/Controllers/newImplementation.php
@@ -62,7 +62,7 @@ class newImplementation extends Controller
 
         $content = json_decode($request->getContent());
         $mpesa_transaction = new PaymentsC2B();
-        $mpesa_transaction->TransactionType = $request;
+        $mpesa_transaction->TransAmount = $content->Amount;
         // $mpesa_transaction->TransID = $content->TransID;
         // $mpesa_transaction->TransTime = $content->TransTime;
         // $mpesa_transaction->TransAmount = $content->TransAmount;
