05be91aa8e32ef0bb77ca1d05539ed6c73cf377b
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-09-07 12:51:00 +0300

    Added changes

--- app/Http/Controllers/newImplementation.php
+++ app/Http/Controllers/newImplementation.php
@@ -62,7 +62,7 @@ class newImplementation extends Controller
 
         $content = json_decode($request->getContent());
         $mpesa_transaction = new PaymentsC2B();
-        $mpesa_transaction->TransAmount = $content->stkCallback->MerchantRequestID;
+        $mpesa_transaction->TransAmount = $content->Body->stkCallback->MerchantRequestID;
         // $mpesa_transaction->TransID = $content->TransID;
         // $mpesa_transaction->TransTime = $content->TransTime;
         // $mpesa_transaction->TransAmount = $content->TransAmount;
