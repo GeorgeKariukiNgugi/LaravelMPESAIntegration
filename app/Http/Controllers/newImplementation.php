bef670b19b2f44f99aba6b5a5f78d3fdc72c05a9
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-09-12 18:14:23 +0300

    Added new change

--- app/Http/Controllers/newImplementation.php
+++ app/Http/Controllers/newImplementation.php
@@ -79,7 +79,7 @@ class newImplementation extends Controller
         // $mpesa_transaction->LastName = $content->LastName;        
         $mpesa_transaction->save();
        
-        Storage::put('attempt3.txt',gettype($content) );
+        Storage::put('attempt3.txt',"Sent." );
         // ! fire the broadcast events. 
         event(new PaymentEvent($content));
 
