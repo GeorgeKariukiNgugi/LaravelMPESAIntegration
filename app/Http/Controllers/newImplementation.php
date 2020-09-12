cedd732fe97873cf95ab33385598292f7da3eed3
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-09-12 18:41:04 +0300

    Added new change

--- app/Http/Controllers/newImplementation.php
+++ app/Http/Controllers/newImplementation.php
@@ -59,7 +59,7 @@ class newImplementation extends Controller
     }
 
     public function callBackForTheSTKPush(Request $request){
-
+        $content = json_decode($request->getContent());
         $mpesa_transaction = new PaymentsC2B();
         // $mpesa_transaction->TransAmount = $content->Body->stkCallback->MerchantRequestID;
         $mpesa_transaction->TransID = $content->Body->stkCallback->MerchantRequestID;
